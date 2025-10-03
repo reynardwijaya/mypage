f2055a0c3bbe718079298b0cd69baa7d42dd1b3c
reynardwijaya <reynardwijaya710@gmail.com>
2025-10-03 09:25:28 +0700

    push lagi

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
       <footer className="w-full bg-zinc-900 dark:bg-zinc-200 py-4 text-center">
         <p className="text-sm text-zinc-400 dark:text-zinc-700">
-          © {new Date().getFullYear()} Reynard Wijaya's Portofolio — All rights reserved.
+          © {new Date().getFullYear()} Reynard Wijaya Portofolio — All rights reserved.
         </p>
       </footer>
     )
